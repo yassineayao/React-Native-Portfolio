0b0a54c3d6e2e1142e3f55770b3225699556172d
Yassine <yassineayao@gmail.com>
2022-11-12 21:33:20 +0100

    Add translations

---
+++ سلعتي/Sel3ti/src/locales/i18n.ts
@@ -0,0 +1,14 @@
+import { I18n } from "i18n-js";
+
+import ar from "./ar.json";
+import en from "./en.json";
+
+const translation = {
+  ar: ar.translation,
+  en: en.translation,
+};
+const i18n = new I18n(translation);
+i18n.enableFallback = true;
+i18n.locale = "ar";
+
+export default i18n;
