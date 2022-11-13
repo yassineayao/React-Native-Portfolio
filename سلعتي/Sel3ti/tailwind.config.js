a6ab13fcbe5fa567eb8379051fa1109dad768e8e
Yassine <yassineayao@gmail.com>
2022-11-14 00:17:10 +0100

    Add custom colors to Tailwind

--- سلعتي/Sel3ti/tailwind.config.js
+++ سلعتي/Sel3ti/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#e65100",
+        lightPrimary: "rgba(230, 81, 0, .7)",
+        secondary: "#bdbdbd",
+        success: "#2a9d8f",
+        lightSuccess: "rgba(42, 157, 143, .3)",
+        danger: "#ef5350",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
